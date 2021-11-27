4aba117f9cf10cebefa99e753dc6909c1f5c15e0
Jake Raw <jtraw96@gmail.com>
2021-11-27 16:11:46 -0600

    added animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
 import Books from "./pages/Books/Books";
@@ -9,18 +11,36 @@ import Navbar from "./components/Navbar/Navbar";
 
 import "./App.scss";
 
+const AnimatedSwitch = () => {
+	const location = useLocation();
+
+	return (
+		<div
+			className={`${
+				location.pathname !== "/" ? "not-home" : ""
+			} app-container `}
+		>
+			<TransitionGroup className='page'>
+				<CSSTransition key={location.key} classNames='fade' timeout={200}>
+					<Routes>
+						<Route path='/' element={<Home />} key={location.key} />
+						<Route path='/about' element={<About />} key={location.key} />
+						<Route path='/books' element={<Books />} key={location.key} />
+						<Route path='/travel' element={<Travel />} key={location.key} />
+						<Route path='*' element={<Error />} key={location.key} />
+					</Routes>
+				</CSSTransition>
+			</TransitionGroup>
+		</div>
+	);
+};
+
 function App() {
 	return (
 		<div className='app'>
 			<BrowserRouter>
 				<Navbar />
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/books' element={<Books />} />
-					<Route path='/travel' element={<Travel />} />
-					<Route path='*' element={<Error />} />
-				</Routes>
+				<AnimatedSwitch />
 			</BrowserRouter>
 		</div>
 	);
